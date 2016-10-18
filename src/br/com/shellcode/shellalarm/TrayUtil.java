1dc8a76075bfdb69848fb8535ee92ab22519b2ca
anderson.calixto <anderson.calixto@ebserh.gov.br>
2016-10-18 13:25:00 -0300

    mensagens do tray

--- src/br/com/shellcode/shellalarm/TrayUtil.java
+++ src/br/com/shellcode/shellalarm/TrayUtil.java
@@ -1,8 +1,6 @@
 package br.com.shellcode.shellalarm;
 
 import java.awt.AWTException;
-import java.awt.CheckboxMenuItem;
-import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
@@ -44,15 +42,6 @@ public class TrayUtil {
 			trayIcon.setImageAutoSize(true);
 			final SystemTray tray = SystemTray.getSystemTray();
 
-			// Create a pop-up menu components
-			MenuItem aboutItem = new MenuItem("About");
-			CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-			CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-			Menu displayMenu = new Menu("Display");
-			MenuItem errorItem = new MenuItem("Error");
-			MenuItem warningItem = new MenuItem("Warning");
-			MenuItem infoItem = new MenuItem("Info");
-			MenuItem noneItem = new MenuItem("None");
 			MenuItem exitItem = new MenuItem("Sair");
 			exitItem.addActionListener(new ActionListener() {
 				@Override
@@ -60,18 +49,6 @@ public class TrayUtil {
 					System.exit(0);
 				};
 			});
-
-			// Add components to pop-up menu
-			popup.add(aboutItem);
-			popup.addSeparator();
-			popup.add(cb1);
-			popup.add(cb2);
-			popup.addSeparator();
-			popup.add(displayMenu);
-			displayMenu.add(errorItem);
-			displayMenu.add(warningItem);
-			displayMenu.add(infoItem);
-			displayMenu.add(noneItem);
 			popup.add(exitItem);
 
 			trayIcon.setPopupMenu(popup);
